8368ff7250440fac99f91c835ff1a0a2b104c39a
Jongwon Lee <jlee@colby.edu>
2018-07-16 16:08:44 -0400

    texteditor

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import {Editor, EditorState, RichUtils, convertToRaw} from 'draft-js';
 
 export default class App extends React.Component {
-  render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+
+  constructor(props){
+    super(props);
+    this.state ={
+      editorState: EditorState.createEmpty()};
+    }
+
+    onChange(editorState) {
+      this.setState({editorState});
+    }
+
+    makeBold(){
+      this.onChange(RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        'BOLD'
+      ));
+    }
+
+    render() {
+      // const raw = convertToRaw(this.state.editorState.getCurrentContent());
+      return <div>
+        <button onClick={() => {this.makeBold();}}>Bold</button>
+        <Editor
+          onChange={(editorState) => {this.onChange(editorState)}}
+          editorState={this.state.editorState}
+        />
+        {/* <div>
+          {JSON.stringify(raw)}
+        </div> */}
+        {/* above is for raw data  */}
+      </div>;
+    }
   }
-}
